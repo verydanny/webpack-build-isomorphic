a975dc7551c8b2533cfdefeb5a3e552930bb7bbe
Daniil Veremchuk <daniel@verydanny.com>
2019-02-05 13:13:56 -0500

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const webpackIsomorphicCompiler = require('webpack-isomorphic-compiler')
+const startReporting = require('webpack-isomorphic-compiler-reporter')
+const startNotifying = require('webpack-sane-compiler-notifier')
+const { compose } = require('compose-middleware')
+const merge = require('lodash.merge')
+const omitBy = require('lodash.omitby')
+const castArray = require('lodash.castarray')
+
+function parseArgs() {
+  const [first = {}, second, third] = args
+
+}
+
+function buildWebpackForExpress(...args) {
+
+}
